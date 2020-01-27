d05c5d4fb41cc3c12c957d4967e148168fdff907
Prashant Ghimire <Ashish@Users-iMac.local>
2020-01-27 19:27:44 +1100

    commit

--- firstApp/blog/views.py
+++ firstApp/blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 
 
+posts = [
+    {
+        'author': 'Ashish',
+        'title': 'Blog Post 1',
+        'content': 'This is my first django app', 
+        'date_posted':'July 1'
+    },
+    {
+        'author': 'Ashish',
+        'title': 'Blog Post 2',
+        'content': 'This is my second django app', 
+        'date_posted':'July 2'
+    }
+]
+
 def home(request):
-    return render(request,'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request,'blog/home.html', context)
 
 
 def about(request):
